f7248c47e0445df2a89714bb5909b0bc3563c135
bognik002 <bognik002@gmail.com>
2023-07-13 03:43:47 +0600

    update Simulator

--- AgentBasedModel/simulator.py
+++ AgentBasedModel/simulator.py
@@ -13,27 +13,29 @@ class Simulator:
     """
     Simulator is responsible for launching agents' actions and executing scenarios
     """
+    # todo: Only traders are to be passed, trader -> trader.exchange -> trader.exchange.asset. Use dict comprehension to get unique items.
     def __init__(
             self,
-            asset:    Type[Asset],
-            exchange: ExchangeAgent = None,
-            traders:  List[Type[Trader]] = None,
-            events:   List[Type[Event]] = None
-        ) -> object:
-        self.asset = asset
-        self.exchange = exchange
-        self.events = [event.link(self) for event in events] if events else None  # link all events to simulator
+            assets:    List[Type[Asset]],
+            exchanges: List[ExchangeAgent],
+            traders:   List[Type[Trader]],
+            events:    List[Type[Event]] = None
+        ):
+        self.assets = assets
+        self.exchanges = exchanges
         self.traders = traders
-        self.info = SimulatorInfo(self.exchange, self.traders)  # links to existing objects
+        self.events = [event.link(self) for event in events] if events else None  # link all events to simulator
+
+        self.info = SimulatorInfo(exchanges, traders)
 
     def _payments(self):
         for trader in self.traders:
             # Dividend payments
-            trader.cash += trader.assets * self.exchange.dividend()  # allows negative assets
+            trader.cash += trader.assets * trader.market.dividend()  # allows negative assets
             # Interest payment
-            trader.cash += trader.cash * self.exchange.risk_free_rate  # allows risk-free loan
+            trader.cash += trader.cash * trader.market.risk_free_rate  # allows risk-free loan
 
-    def simulate(self, n_iter: int, silent=False) -> object:
+    def simulate(self, n_iter: int, silent: bool = False) -> object:
         for it in tqdm(range(n_iter), desc='Simulation', disable=silent):
             # Call scenario
             if self.events:
@@ -43,21 +45,21 @@ class Simulator:
             # Capture current info
             self.info.capture()
 
-            # Change behaviour
+            # Call Traders
+            random.shuffle(self.traders)
+
             for trader in self.traders:
                 if type(trader) == Universalist:
                     trader.change_strategy(self.info)
                 if type(trader) in (Universalist, Chartist):
                     trader.change_sentiment(self.info)
 
-            # Call Traders
-            random.shuffle(self.traders)
-            for trader in self.traders:
                 trader.call()
 
             # Payments and dividends
             self._payments()
-            self.asset.update()  # generate next dividends
+            for asset in self.assets:
+                asset.update()  # generate next dividend
 
         return self
 
@@ -66,23 +68,29 @@ class SimulatorInfo:
     """
     SimulatorInfo is responsible for capturing data during simulating
     """
-    def __init__(self, exchange: ExchangeAgent = None, traders: list = None) -> object:
-        self.exchange = exchange
-        self.traders = {t.id: t for t in traders}
+    def __init__(
+            self,
+            exchanges: List[ExchangeAgent],
+            traders:   List[Type[Trader]],
+        ):
+        self.exchanges = {exchange.id: exchange for exchange in exchanges}
+        self.traders   = {trader.id: trader for trader in traders}
 
         # Market Statistics
-        self.prices = list()  # price at the end of iteration
-        self.spreads = list()  # bid-ask spreads
-        self.dividends = list()  # dividend paid at each iteration
-        self.orders = list()  # order book statistics
+        self.prices    = {idx: list() for idx in self.exchanges.keys()}  # exchange: price at the end of iteration
+        self.spreads   = {idx: list() for idx in self.exchanges.keys()}  # exchange: bid-ask spreads
+        self.dividends = {idx: list() for idx in self.exchanges.keys()}  # exchange: dividend paid at each iteration
+        self.orders    = {idx: list() for idx in self.exchanges.keys()}  # exchange: order book statistics
 
         # Agent statistics
-        self.equities = list()  # agent: equity
-        self.cash = list()  # agent: cash
-        self.assets = list()  # agent: number of assets
-        self.types = list()  # agent: current type
-        self.sentiments = list()  # agent: current sentiment
-        self.returns = [{tr_id: 0 for tr_id in self.traders.keys()}]  # agent: iteration return
+        self.cash       = {idx: list() for idx in self.traders.keys()}  # trader: cash
+        self.assets     = {idx: list() for idx in self.traders.keys()}  # trader: number of assets
+        self.equities   = {idx: list() for idx in self.traders.keys()}  # trader: equity
+        self.types      = {idx: list() for idx in self.traders.keys()}  # trader: current type
+        self.sentiments = {idx: list() for idx in self.traders.keys()}  # trader: current sentiment
+        self.returns    = {idx: list() for idx in self.traders.keys()}  # trader: return (price change + dividend)
+
+        # self.returns = [{tr_id: 0 for tr_id in self.traders.keys()}]  # agent: return (price change + dividend)
 
     def capture(self):
         """
@@ -92,83 +100,148 @@ class SimulatorInfo:
 
         *Market Statistics*
 
-        - :class:`list[float]` **prices** --> stock prices on each iteration
-        - :class:`list[dict]` **spreads** --> order book spreads on each iteration
-        - :class:`list[float]` **dividends** --> dividend paid on each iteration
-        - :class:`list[dict[dict]]` **orders** --> order book price, volume, quantity stats on each iteration
+        - :class:`dict[list[float]]` **prices** --> stock prices on each iteration
+        - :class:`dict[list[dict]]` **spreads** --> order book bid-ask pairs on each iteration
+        - :class:`dict[list[float]]` **dividends** --> dividend paid on each iteration
+        - :class:`dict[list[dict[dict]]]` **orders** --> order book price, volume, quantity stats on each iteration
 
         *Traders Statistics*
 
-        - :class:`list[dict]` **equities** --> each agent's equity on each iteration
-        - :class:`list[dict]` **cash** --> each agent's cash on each iteration
-        - :class:`list[dict]` **assets** --> each agent's number of stocks on each iteration
-        - :class:`list[dict]` **types** --> each agent's type on each iteration
+        - :class:`dict[list[dict]]` **equities** --> each agent's equity on each iteration
+        - :class:`dict[list[dict]]` **cash** --> each agent's cash on each iteration
+        - :class:`dict[list[dict]]` **assets** --> each agent's number of stocks on each iteration
+        - :class:`dict[list[dict]]` **types** --> each agent's type on each iteration
         """
+
         # Market Statistics
-        self.prices.append(self.exchange.price())
-        self.spreads.append((self.exchange.spread()))
-        self.dividends.append(self.exchange.dividend())
-        self.orders.append({
-            'quantity': {'bid': len(self.exchange.order_book['bid']), 'ask': len(self.exchange.order_book['ask'])},
-            # 'price mean': {
-            #     'bid': mean([order.price for order in self.exchange.order_book['bid']]),
-            #     'ask': mean([order.price for order in self.exchange.order_book['ask']])},
-            # 'price std': {
-            #     'bid': std([order.price for order in self.exchange.order_book['bid']]),
-            #     'ask': std([order.price for order in self.exchange.order_book['ask']])},
-            # 'volume sum': {
-            #     'bid': sum([order.qty for order in self.exchange.order_book['bid']]),
-            #     'ask': sum([order.qty for order in self.exchange.order_book['ask']])},
-            # 'volume mean': {
-            #     'bid': mean([order.qty for order in self.exchange.order_book['bid']]),
-            #     'ask': mean([order.qty for order in self.exchange.order_book['ask']])},
-            # 'volume std': {
-            #     'bid': std([order.qty for order in self.exchange.order_book['bid']]),
-            #     'ask': std([order.qty for order in self.exchange.order_book['ask']])}
-        })
+        for idx, exchange in self.exchanges.items():
+            self.prices[idx].append(exchange.price())
+            self.spreads[idx].append(exchange.spread())
+            self.dividends[idx].append(exchange.dividend())
+
+            # Order book details
+            self.orders[idx].append({
+                'quantity': {
+                    'bid': len(exchange.order_book['bid']),
+                    'ask': len(exchange.order_book['ask'])
+                },
+                # 'price mean': {
+                #     'bid': mean([order.price for order in self.exchange.order_book['bid']]),
+                #     'ask': mean([order.price for order in self.exchange.order_book['ask']])
+                # },
+                # 'price std': {
+                #     'bid': std([order.price for order in self.exchange.order_book['bid']]),
+                #     'ask': std([order.price for order in self.exchange.order_book['ask']])
+                # },
+                # 'volume sum': {
+                #     'bid': sum([order.qty for order in self.exchange.order_book['bid']]),
+                #     'ask': sum([order.qty for order in self.exchange.order_book['ask']])
+                # },
+                # 'volume mean': {
+                #     'bid': mean([order.qty for order in self.exchange.order_book['bid']]),
+                #     'ask': mean([order.qty for order in self.exchange.order_book['ask']])
+                # },
+                # 'volume std': {
+                #     'bid': std([order.qty for order in self.exchange.order_book['bid']]),
+                #     'ask': std([order.qty for order in self.exchange.order_book['ask']])
+                # }
+            })
 
         # Trader Statistics
-        self.equities.append({t_id: t.equity() for t_id, t in self.traders.items()})
-        self.cash.append({t_id: t.cash for t_id, t in self.traders.items()})
-        self.assets.append({t_id: t.assets for t_id, t in self.traders.items()})
-        self.types.append({t_id: t.type for t_id, t in self.traders.items()})
-        self.sentiments.append({t_id: t.sentiment for t_id, t in self.traders.items() if t.type == 'Chartist'})
-        self.returns.append({tr_id: (self.equities[-1][tr_id] - self.equities[-2][tr_id]) / self.equities[-2][tr_id]
-                             for tr_id in self.traders.keys()}) if len(self.equities) > 1 else None
+        for idx, trader in self.traders.items():
+            self.cash[idx].append(trader.cash)
+            self.assets[idx].append(trader.assets)
+            self.equities[idx].append(trader.equity())
+            self.types[idx].append(trader.type)
 
-    def fundamental_value(self, access: int = 0) -> list:
-        divs = self.dividends.copy()
-        n = len(divs)  # number of iterations
-        divs.extend(self.exchange.dividend(access)[1:access+1])  # current div is already recorded
-        r = self.exchange.risk_free_rate
+            if len(self.equities[idx]) > 1:
+                self.returns[idx].append(
+                    (self.equities[idx][-1] - self.equities[idx][-2]) / self.equities[idx][-2]
+                )
+            if type(trader) in (Chartist, Universalist):
+                self.sentiments[idx].append(trader.sentiment)
 
-        return [Fundamentalist.evaluate(divs[i:i+access+1], r) for i in range(n)]
+    def fundamental_value(self, idx: int, access: int = 0) -> list:
+        """Fundamental value is calculated using Fundamental trader logic.
 
-    def stock_returns(self, roll: int = None) -> list or float:
-        p = self.prices
-        div = self.dividends
-        r = [(p[i+1] - p[i]) / p[i] + div[i] / p[i] for i in range(len(p) - 1)]
-        return rolling(r, roll) if roll else mean(r)
+        :param idx: ExchangeAgent id
+        :param access: Fundamentalist's number of known dividends, defaults to 0
+        :return: stock fundamental values
+        """
+        rf = self.exchanges[idx].risk_free_rate
+        divs = [
+            *self.dividends[idx],                              # paid dividends
+            *self.exchanges[idx].dividend(access)[1:access+1]  # unpaid dividends
+        ]
 
-    def abnormal_returns(self, roll: int = None) -> list:
-        rf = self.exchange.risk_free_rate
-        r = [r - rf for r in self.stock_returns()]
-        return rolling(r, roll) if roll else r
+        return [Fundamentalist.evaluate(divs[i:i+access+1], rf) for i in range(len(divs) - access)]
 
-    def return_volatility(self, window: int = None) -> list or float:
-        if window is None:
-            return std(self.stock_returns())
-        n = len(self.stock_returns(1))
-        return [std(self.stock_returns(1)[i:i+window]) for i in range(n - window)]
+    def stock_returns(self, idx: int, roll: int = 1) -> list:
+        """Stock return is calculated based on: 1) dividend, 2) price change
 
-    def price_volatility(self, window: int = None) -> list or float:
-        if window is None:
-            return std(self.prices)
-        return [std(self.prices[i:i+window]) for i in range(len(self.prices) - window)]
+        :param idx: ExchangeAgent id
+        :param roll: MA applied to list, defaults to 1
+        :return: stock returns
+        """
+        p = self.prices[idx]
+        div = self.dividends[idx]
+        
+        r = [
+              (p[i+1] - p[i]) / p[i]  # price related return
+            + (div[i]) / p[i]         # dividend related return
 
-    def liquidity(self, roll: int = None) -> list or float:
-        n = len(self.prices)
-        spreads = [el['ask'] - el['bid'] for el in self.spreads]
-        prices = self.prices
-        liq = [spreads[i] / prices[i] for i in range(n)]
-        return rolling(liq, roll) if roll else mean(liq)
+            for i in range(len(p) - 1)
+        ]
+
+        return rolling(r, roll)
+
+    def abnormal_returns(self, idx: int, roll: int = 1) -> list:
+        """Stock abnormal return is a return that is greater than risk-free rate
+
+        :param idx: ExchangeAgent id
+        :param roll: MA applied to list, defaults to 1
+        :return: stock abnormal returns
+        """
+        rf = self.exchanges[idx].risk_free_rate
+        
+        r = [r - rf for r in self.stock_returns(idx)]
+
+        return rolling(r, roll)
+
+    def return_volatility(self, idx: int, window: int = 5) -> list:
+        """Stock return volatility is calculated as a standard deviation of stock returns 
+
+        :param idx: ExchangeAgent id
+        :param window: sample size, > 1, defaults to 1
+        :return: stock return volatility
+        """
+
+        r = self.stock_returns(idx)
+
+        return [std(r[i:i+window]) for i in range(len(r) - window + 1)]
+
+    def price_volatility(self, idx: int, window: int = 5) -> list:
+        """Stock price volatility is calculated as a standard deviation of stock market prices 
+
+        :param idx: ExchangeAgent id
+        :param window: sample size, > 1, defaults to 1
+        :return: stock price volatility
+        """
+        
+        p = self.prices[idx]
+
+        return [std(p[i:i+window]) for i in range(len(p) - window + 1)]
+
+    def liquidity(self, idx: int, roll: int = 1) -> list:
+        """Stock liquidity is calculated as a best-ask, best-bid difference relative to market price
+
+        :param idx: ExchangeAgent id
+        :param roll: MA applied to list, defaults to 1
+        :return: stock liquidities
+        """
+        prices = self.prices[idx]
+        spreads = [el['ask'] - el['bid'] for el in self.spreads[idx]]
+
+        liq = [spreads[i] / prices[i] for i in range(len(prices))]
+
+        return rolling(liq, roll)
